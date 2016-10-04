f744b66d09725fa59604b3de08a655a58a3944fd
Daniel Gallik <gallikdaniel@gmail.com>
2016-10-04 23:26:01 +0200

    Created project

---
+++ src/MyDebtors/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDebtors.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
